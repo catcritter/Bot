600358ccec5da29461cc282a4e139e029c86afe4
(≧◡≦)Kanna(≧◡≦) <shenmcginty@gmail.com>
2018-07-16 13:46:18 +1000

    botinfo udate

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -8,6 +8,7 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
     .addField("Created On", bot.user.createdAt);
+    .addField("Server count", bot.guilds.size)
 
     message.channel.send(botembed);
 }
