8b024204f223697ae2688675f260c84bc73e57ff
(≧◡≦)Kanna(≧◡≦) <shenmcginty@gmail.com>
2018-07-17 13:20:39 +1000

    test

--- commands/coins.js
+++ commands/coins.js
@@ -17,7 +17,7 @@ module.exports.run = async (bot, message, args) => {
   .setColor("#00FF00")
   .addField("💸", uCoins);
 
-  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+  message.channel.send(coinEmbed)
 
 }
 
