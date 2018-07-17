28f1c41aa7fd1bb9436922ca6e1129f88388664a
(≧◡≦)Kanna(≧◡≦) <shenmcginty@gmail.com>
2018-07-17 12:24:17 +1000

    fixx

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -29,7 +29,6 @@ module.exports.run = async (client, message, args) => {
 		.setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
      message.channel.send({embed});
     }
-}
 
 module.exports.help = {
 name: "userinfo"
